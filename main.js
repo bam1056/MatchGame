2f6ee1c8091b446de5988e7ff673c3db49e029b7
Brett Macy <bam03584@aol.com>
2016-08-04 23:44:41 -0400

    removed event click

--- main.js
+++ main.js
@@ -46,18 +46,6 @@ const init = () => {
       })
     }
   }
-  // drawBoard()
-  // let bashir = document.querySelector('.one1')
-
-  // bashir.addEventListener('click', () => {
-  //   if (firstClick){
-  //     bashir.style.backgroundImage = "url('./images/bashir.jpeg')"
-  //     firstClick = false
-  //   } else {
-  //     bashir.style.backgroundImage = "url('./images/spaceBG.jpeg')"
-  //     firstClick = true
-  //   }
-  // })
 }
 
 document.addEventListener('DOMContentLoaded', init)
