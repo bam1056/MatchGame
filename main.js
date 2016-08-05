3c1dbdd5a3b79fdeac61292ee890799465ea4fc3
Brett Macy <bam03584@aol.com>
2016-08-04 23:41:41 -0400

    random images inserted into the gameboard

--- main.js
+++ main.js
@@ -6,25 +6,40 @@ const state = [
   [0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0]
 ]
-let firstClick = true
-const cellValues = {0:"url('./images/spaceBG.jpeg')"}
+// let firstClick = true
+const cellValues = { 0: "url('./images/spaceBG.jpeg')" }
+let numArray = []
+
+const setNumArray = () => {
+  for (let i = 0; i < 36; i++) {
+    numArray.push(i)
+  }
+  return numArray
+}
+
+const getImageNum = () => {
+  var randomIndex = Math.floor(Math.random() * numArray.length)
+   return numArray.splice(randomIndex, 1)[0]
+}
 
 const drawBoard = () => {
+  let imageNum
   for (let i = 0; i < state.length; i++) {
     for (let j = 0; j < state[i].length; j++) {
-      const col = state[i][j]
+      // const col = state[i][j]
       const cell = document.querySelector(
-        `table tr:nth-child(${i+1}) td:nth-child(${j+1})`
+        `table tr:nth-child(${i + 1}) td:nth-child(${j + 1})`
       )
-      //console.log("Winning", cellValues[col],cell)
-      //cell.textContent = "HELP ME!!!"
-      cell.style.backgroundImage = cellValues[col]
+      imageNum = getImageNum()
+      console.log("Img num", imageNum)
+      cell.style.backgroundImage = `url('./images/img00${imageNum + 1}.jpeg')`
     }
-    //console.log('After the for loop:',cell)
   }
 }
 
 const init = () => {
+  setNumArray()
+  console.log(numArray)
   drawBoard()
   const rows = document.querySelectorAll('tr')
   for (let i = 0; i < rows.length; i++) {
@@ -36,7 +51,7 @@ const init = () => {
     }
   }
   // drawBoard()
-  //let bashir = document.querySelector('.one1')
+  // let bashir = document.querySelector('.one1')
 
   // bashir.addEventListener('click', () => {
   //   if (firstClick){
